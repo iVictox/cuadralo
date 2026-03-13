814936899ec0bd3787a9d335484f5e43a461d772
Victox <deabreuvictorhugo@gmail.com>
2026-03-13 10:27:08 -0400

    fix timezone

--- backend/database/connect.go
+++ backend/database/connect.go
@@ -18,7 +18,7 @@ func Connect() {
 		log.Fatal("Error loading .env file")
 	}
 
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=America/Caracas",
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=UTC",
 		os.Getenv("DB_HOST"),
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
